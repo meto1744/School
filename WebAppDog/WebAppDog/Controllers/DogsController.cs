68f33aa92e6f1cd49827c9164b0f4fcd5ed62500
meto1744 <meto17@abv.bg>
2023-01-23 19:46:02 +0200

    WebAppDog

--- WebAppDog/WebAppDog/Controllers/DogsController.cs
+++ WebAppDog/WebAppDog/Controllers/DogsController.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebAppDog.Data;
+using WebAppDog.Domain;
+using WebAppDog.Models;
 
 namespace WebAppDog.Controllers
 {
@@ -23,5 +25,52 @@ namespace WebAppDog.Controllers
         {
             return View();
         }
+        [HttpPost]
+        
+        public IActionResult Create(DogCreateViewModel bindingModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Dog dogFromDb = new Dog
+                {
+                    Name = bindingModel.Name,
+                    Age = bindingModel.Age,
+                    Breed = bindingModel.Breed,
+                    Picture = bindingModel.Picture,
+                };
+
+                context.Dogs.Add(dogFromDb);
+                context.SaveChanges();
+
+                return this.RedirectToAction("Success");
+            }
+            return this.View();
+        }
+        public IActionResult Success()
+        {
+            return this.View();
+        }
+        public IActionResult All()
+        {
+            List<DogAllViewModel> dogs = context.Dogs
+                .Select(dogFrpmDb => new DogAllViewModel
+
+                 {
+                    Id = dogFrpmDb.Id,
+                    Name = dogFrpmDb.Name,
+                    Age= dogFrpmDb.Age,
+                    Breed= dogFrpmDb.Breed,
+                    Picture= dogFrpmDb.Picture
+
+                 }
+                 ).ToList();
+
+            return View(dogs);
+        }
+        
     }
+   
 }
+
+
+
---
+++ WebAppDog/WebAppDog/Models/DogAllViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppDog.Models
+{
+    public class DogAllViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Name")]
+
+        public string Name { get; set; }
+
+        [Display(Name = "Age")]
+
+        public int Age { get; set; }
+
+
+        [Display(Name = "Breed")]
+
+        public string Breed { get; set; }
+
+        [Display(Name = "Dog Picture")]
+
+        public string Picture { get; set; }
+    }
+}
