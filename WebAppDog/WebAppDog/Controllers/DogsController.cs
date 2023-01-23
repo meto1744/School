b703c12989f372f971f2b5220692905a00a26ebe
meto1744 <meto17@abv.bg>
2023-01-23 13:16:18 +0200

    WebAppDog

---
+++ WebAppDog/WebAppDog/Controllers/DogsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppDog.Data;
+
+namespace WebAppDog.Controllers
+{
+    public class DogsController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public DogsController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
